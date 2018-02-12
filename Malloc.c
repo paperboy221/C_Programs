43b8c21186bab68a1d2bae66177aba26eed0dfe4
paperboy221 <shatra7@gmail.com>
2018-02-12 16:59:19 -0500

    Update Malloc.c

--- Malloc.c
+++ Malloc.c
@@ -2,17 +2,32 @@
 #include <stdlib.h>
 int main()
 {
+    
+    // int x pointer to variabe
+    int *x = NULL;
+    
+    x = malloc(sizeof(int));
+    *x = 6;
+    printf("x is %d\n", *x);
+    
+    // int array pointer to array
+    int *arr = NULL;
+    arr = malloc(sizeof(int) * 10);
+    
+    int i = 0;
+    for (i=0;i<10;++i){
+        arr[i] = i * 2;
+    }
+    
+    for (i=0;i<10;i++){
+      printf("%d ", arr[i]);
+               }
+               printf("\n");
+   // free space from computer
+    free(x);
+    free(arr);
+               return 0;
+               
+               }
 
-int *x = NULL;
-
-x = malloc(sizeof(int));
-*x = 6;
-printf("x is %d\n", *x);
-
-
-
-return 0;
-
-}
-
-
+               
